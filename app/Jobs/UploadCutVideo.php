b6c5b26bf9adc8c73337f22ff7eb2593725938a3
Andrey Chirskiy <andrey_chirskiy@govitall.com>
2017-03-05 22:35:56 +0200

    removed video cutting implementation from job

--- app/Jobs/UploadCutVideo.php
+++ app/Jobs/UploadCutVideo.php
@@ -2,17 +2,12 @@
 
 namespace App\Jobs;
 
-use App\Helpers\File;
 use App\Models\AppVideo;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use FFMpeg\FFMpeg;
-use Storage;
-use FFMpeg\Format\Video\X264;
-use FFMpeg\Coordinate\TimeCode;
 
 class UploadCutVideo implements ShouldQueue
 {
@@ -24,12 +19,10 @@ class UploadCutVideo implements ShouldQueue
     /**
      * Create a new job instance.
      * @param AppVideo $video
-     * @param $fileUrl
      */
-    public function __construct(AppVideo $video, $fileUrl)
+    public function __construct(AppVideo $video)
     {
         $this->video = $video;
-        $this->fileUrl = $fileUrl;
     }
 
     /**
@@ -41,13 +34,7 @@ class UploadCutVideo implements ShouldQueue
     {
         $this->setVideoStatus(AppVideo::PROCESS_PROCESSING);
 
-        $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => env('FFMPEG_PATH'),
-            'ffprobe.binaries' => env('FFPROBE_PATH')
-        ]);
-        $video = $ffmpeg->open($this->fileUrl);
-        $video->filters()->clip(TimeCode::fromSeconds($this->video->start_time), TimeCode::fromSeconds($this->video->duration));
-        $video->save(new X264(env('FFMPEG_AUDIO_CODEC')), File::setNewName($this->fileUrl, env('CUT_FILE_NAME')));
+        $this->video->cutVideo();
 
         $this->setVideoStatus(AppVideo::PROCESS_DONE);
     }
